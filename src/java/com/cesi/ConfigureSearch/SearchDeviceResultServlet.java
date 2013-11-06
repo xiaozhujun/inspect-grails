9234225546d5fd4defea6e943e4f28fee7a86a63
xiaozhujun <xiaozhujun520@163.com>
2013-11-07 00:11:30 +0800

    go

---
+++ src/java/com/cesi/ConfigureSearch/SearchDeviceResultServlet.java
@@ -0,0 +1,152 @@
+package com.cesi.configuresearch;
+
+import com.springsource.roo.inspect.dao.DbImpl;
+import model.PageInspectTable;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ThinkPad
+ * Date: 13-11-1
+ * Time: 下午4:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class SearchDeviceResultServlet extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out=response.getWriter();
+        String dtype=request.getParameter("dtype");
+        String device=request.getParameter("device");
+        String tag=request.getParameter("tag");
+        String tableTemplate="<form method='post'><table border=1 class='tb1'><thead><tr><th>设备类型</th><th>设备类型编号</th><th>设备编号</th><th>标签区域</th><th>标签号</th><th>标签编号</th><th>操作</th></thead><tbody>";
+        String tfootTemplate="</table></form>";
+        String empty="对不起，查询记录不存在！";
+        DbImpl d=new DbImpl();
+        PageInspectTable p=null;
+        List<PageInspectTable> list=new ArrayList<PageInspectTable>();
+        if(dtype==""&&device==""&&tag==""){
+            out.print("对不起，请输入查询条件！");
+        }else{
+            if(dtype!=""&&device!=""&&tag!=""){
+                int d1=Integer.parseInt(dtype);
+                int dd=Integer.parseInt(device);
+                int t=Integer.parseInt(tag);
+                list=d.getTypeDevTag(d1,dd,t);
+                if(list.size()==0){
+                    out.println(empty);
+                }else{
+                out.println(tableTemplate);
+                Iterator it1=list.iterator();
+                while(it1.hasNext()){
+                    p=(PageInspectTable)it1.next();
+                    out.println(d.returnDeviceResultString(p,dd));
+                }
+                out.println(tfootTemplate);
+                }
+            }else if(device!=""&&tag!=""){
+                int dd=Integer.parseInt(device);
+                int t=Integer.parseInt(tag);
+                list=d.getDevTag(dd,t);
+                if(list.size()==0){
+                 out.println(empty);
+                }else{
+                 out.println(tableTemplate);
+                Iterator it1=list.iterator();
+                while(it1.hasNext()){
+                    p=(PageInspectTable)it1.next();
+                    out.println(d.returnDeviceResultString(p,dd));
+                }
+                out.println(tfootTemplate);
+                }
+            } else if(dtype!=""&&device!=""){
+                int d1=Integer.parseInt(dtype);
+                int dd=Integer.parseInt(device);
+                list=d.getTypeDev(dd,d1);
+                if(list.size()==0){
+                 out.println(empty);
+                }else{
+                 out.println(tableTemplate);
+                Iterator it1=list.iterator();
+                while(it1.hasNext()){
+                    p=(PageInspectTable)it1.next();
+                out.println(d.returnDeviceResultString(p,dd));
+                }
+                out.println(tfootTemplate);
+                }
+            }else if(dtype!=""&&tag!=""){
+                int d1=Integer.parseInt(dtype);
+                int t=Integer.parseInt(tag);
+                list=d.getTypeTag(d1,t);
+                if(list.size()==0){
+                 out.println(empty);
+                }else{
+                 out.println(tableTemplate);
+                Iterator it1=list.iterator();
+                while(it1.hasNext()){
+                    p=(PageInspectTable)it1.next();
+                    out.println(d.returnDeviceResultString(p,p.getDeviceid()));
+                }
+                out.println(tfootTemplate);
+                }
+            }else if(dtype!=""){
+                int d1=Integer.parseInt(dtype);
+                list=d.getType(d1);
+                if(list.size()==0){
+                    out.println(empty);
+                }else{
+                    out.println(tableTemplate);
+                Iterator it1=list.iterator();
+                while(it1.hasNext()){
+                    p=(PageInspectTable)it1.next();
+                    out.println(d.returnDeviceResultString(p,p.getDeviceid()));
+                }
+                out.println(tfootTemplate);
+                }
+            }else if(device!=""){
+                int d1=Integer.parseInt(device);
+                list=d.getDevice(d1);
+                if(list.size()==0){
+                    out.println(empty);
+                }else{
+                    out.println(tableTemplate);
+                Iterator it1=list.iterator();
+                while(it1.hasNext()){
+                    p=(PageInspectTable)it1.next();
+                 out.println(d.returnDeviceResultString(p,d1));
+                }
+                out.println(tfootTemplate);
+                }
+            } else if(tag!=""){
+                int d1=Integer.parseInt(tag);
+                list=d.getTag(d1);
+                if(list.size()==0){
+                    out.println(empty);
+                }else{
+                    out.println(tableTemplate);
+                Iterator it1=list.iterator();
+                while(it1.hasNext()){
+                    p=(PageInspectTable)it1.next();
+                    out.println(d.returnDeviceResultString(p,p.getDeviceid()));
+                 }
+                out.println(tfootTemplate);
+                }
+            }
+        }
+
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      doPost(request,response);
+    }
+}
---
+++ src/java/com/cesi/ConfigureSearch/UserqueryresultServlet.java
@@ -0,0 +1,71 @@
+package com.cesi.configuresearch;
+
+import com.springsource.roo.inspect.dao.DbImpl;
+import model.InspectTableRecord;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+/**
+ * Created with IntelliJ IDEA.
+ * User: ThinkPad
+ * Date: 13-11-1
+ * Time: 下午5:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserQueryResultServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out=response.getWriter();
+        String s = request.getParameter("s");
+        String e =  request.getParameter("e");
+        String t =  request.getParameter("t");
+        String type =  request.getParameter("type");
+        String result="<div id='title2'>查询结果:</div>";
+        String tableTemplate="<table border='0'  cellpadding='0' cellspacing='1' bgcolor='#000000' class='querytable'>" +
+          "<thead><tr bgcolor='#FFFFFF'><th class='tm'>点检表</th><th>点检人员</th>" +
+         " <th class='tm'>点检时间</th><th colspan='2' class='tm'>操作</th></tr></thead><tbody><tr bgcolor='#FFFFFF'>";
+        String empty="对不起!查询记录不存在!";
+        InspectTableRecord r;
+        if (s != null && e != null && t != null && type != null) {
+            int tid = Integer.parseInt(t);
+            SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
+            Date d11=null;
+            Date d2=null;
+            try{
+                    d11= sf.parse(s);
+                    d2= sf.parse(e);
+            }catch (ParseException e1){
+                e1.printStackTrace();
+            }
+            DbImpl d = new DbImpl();
+            List<InspectTableRecord> l11 = d.getR(d11, d2, tid);
+            if(l11.size()==0){
+                out.println(empty);
+            }else {
+            out.println(tableTemplate);
+            Iterator it = l11.iterator();
+            while (it.hasNext()) {
+                r = (InspectTableRecord) it.next();
+                out.print(d.returnUserQueryResultTableString(r));
+            }
+            }
+         }else if(s==null||e==null){
+               out.println("<div id='title1'>请输入查询条件!</div>");
+         }
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+           doPost(request,response);
+    }
+
+}
